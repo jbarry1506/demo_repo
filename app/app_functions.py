c2968f3a81801a8b81aee6ecbfa4dddc5f55723b
Jim Barry <jbarry1506@gmail.com>
2024-10-27 12:26:03 -0400

    added app_functions.py

---
+++ app/app_functions.py
@@ -0,0 +1,17 @@
+import math
+
+def add_func(num1: int, num2: int):
+    """
+    This function adds together 2 integers.
+    num1 requires int
+    num2 requires int
+    """
+    num_output = math.add(num1, num2)
+
+    return num_output
+
+
+
+added_nums = add_func(4,5)
+
+print(added_nums)
